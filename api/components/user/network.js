e97c310367b6fa1f0c43ef247d9f48f55e7c01f7
Camilo Guzman <camiloge_91@hotmail.com>
2021-01-25 22:55:37 -0500

    Class_7

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,12 +1,28 @@
 const express = require('express');
 
 const response = require('../../../network/response');
+const Controller = require('./index');
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send('All working');
-  response.success(req, res, 'Todo correcto', 200);
+  Controller.list()
+    .then((lista) => {
+      response.success(req, res, lista, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500);
+    });
+});
+
+router.get('/:id', function (req, res) {
+  Controller.get(req.params.id)
+    .then((user) => {
+      response.success(req, res, user, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500);
+    });
 });
 
 module.exports = router;
